89fd1cdbcf6bdfd05c235a45fcddd293605d2901
vbraraujo <vbraraujo@yahoo.com.br>
2021-03-20 21:33:56 -0300

    Inclusão de clientes em lote e unificação de arquivo de pendências

--- cadastrar_clientes_lote.sh
+++ cadastrar_clientes_lote.sh
@@ -1,76 +1,35 @@
 #! /bin/bash
 
-PATH_BASE="$HOME/Dropbox/RA-Compartilhamento/crm-cli"
+#PATH_BASE="/mnt/Arquivos/Downloads/fichas"
+PATH_BASE="/mnt/Arquivos/Downloads/teste"
 
 PATH_CLIENTES_LISTA="${PATH_BASE}/clientes/.lista"
-PATH_FUNIS="${PATH_BASE}/modulos"
-MENU_INICIAL="Pesquisar cliente\nCadastrar cliente\nRelatórios\nSair"
 DATA_HOJE="$(printf '%(%Y-%m-%d)T\n' -1)"
-DATA_UMA_SEMANA="$(date --date="1 week" +"%Y-%m-%d")"
-LISTA_FUNIL="$(find "${PATH_FUNIS}/" -name "*.funil" -printf "%f\n")"
 
 limpa_numeros(){
     local tmp=${1//./}
     echo ${tmp//-/}
 }
 
-cadastrar_cliente(){
-    nome="${1}"
-    cpf="${2}"
-    telefone="${3}"
-    email="${4}"
-    etiquetas="metro"
-    status="Quente"
+criar_ficha_cadastro(){
 
-    # Organiza e cria estrutura para pasta do cliente
-    destino="${PATH_BASE}/clientes/${cpf}"
-    arquivo_principal="Nome: ${nome}\nCPF: ${cpf}\nTelefone: ${telefone}\nE-mail: ${email}\nStatus: ${status}\nEtiquetas: ${etiquetas}"
-    cliente_lista="${nome}|${cpf}|${telefone}|${email}|${etiquetas}|${status}"
-
-    mkdir "${destino}/"
-    echo -e "$arquivo_principal" > "${destino}/cadastro"
-    echo -e "$cliente_lista" >> "${PATH_BASE}/clientes/.lista"
-    cadastrar_historico "$cpf" "Cliente cadastrado" "#manual"
-
-    touch "${destino}/pendencias"
-    touch "${destino}/funis"
-
-    mkdir "${destino}/mails"
-    mkdir "${destino}/mails/pendentes"
-    mkdir "${destino}/mails/enviados"
-
-    echo "$(tput setaf 2)[info] Cliente cadastrado com sucesso.$(tput sgr0)"
+    while read line; do
+        cadastro_content="$(echo "$line" | awk -F"';'" '{ if ( $11 == "DESLIGADO" ) { tag = "ex-metro" }
+        else { tag = "metro" }
+            print "Nome: "$2"\nCPF: "$27"\nTelefone: "$21"\nE-mail: \nStatus: Neutro\nEtiquetas: "tag"\nEstado civil: "$29"\nProfissão: metroviário(a)\nRG: "$23"\nEndereço: "$16, $17, $18, $19 "\nCEP: "$20"\nMatrícula: "$1"\nCargo: "$3"\nFunção gratificada: "$4"\nSituação funcional: "$5"\nHoras base mensal: "$6"\nHoras base semanal: "$7"\nHoras base acordo: "$8"\nPericulosidade: "$9"\nInsalubridade: "$10"\nSituação: "$11"\nData de exon/dem/falecimento: "$13"\nData de nascimento: "$14"\nData de admissão: "$15"\nEscolaridade: "$22"\nTitulo eleitoral: "$24"\nNaturalidade: "$25"\nSexo: "$26"\nPIS/PASEP: "$27"\nConjuge: "$30"\nPai: "$31"\nMãe: "$32"\nDados bancários: "$33 
+        }')"
+        cpf="$( awk -F"';'" '{ print $27 }' <<< "$line" )"
+        destino="$HOME/Downloads/fichas/clientes/${cpf}"
+        echo "$destino"
+        [[ -d $destino ]] && echo -e "$cadastro_content" > "${destino}/cadastro"
+    done < "${1}"
 }
 
-cadastrar_historico(){
-    #Argumentos: 1 CPF, 2 descrição, 3 tags
-    if [ "$#" -eq 3 ]
-        then
-            path_hist="${PATH_BASE}/clientes/${1}/historico"
-            linha="$DATA_HOJE: $2 $3"
-            echo -e "$linha" >> "$path_hist"
-
-        else
-            echo "Não foi possível criar anotação automática. Favor informar os dados a seguir."
-            read -p "Descrição: " descricao
-            read -p "CPF: " cpf
-            read -p "Tags: " tags
-            path_hist="${PATH_BASE}/clientes/${cpf}/historico"
-            linha="$DATA_HOJE: $descricao $tags"
-            echo -e "$linha" >> "$path_hist"
-    fi
+completar_cadastro () {
+    for folder in ./* ; do
+        echo -e "Matrícula: \nCargo: \nFunção gratificada: \nSituação funcional: \nHoras base mensal: \nHoras base semanal: \nHoras base acordo: \nPericulosidade: \nInsalubridade: \nSituação: \nData de exon/dem/falecimento: \nData de nascimento: \nData de admissão: \nEscolaridade: \nTitulo eleitoral: \nNaturalidade: \nSexo: \nPIS/PASEP: \nConjuge: \nPai: \nMãe: \nDados bancários: " >> "${folder}/cadastro"
+    done 
 }
 
-cadastrar_contatos(){
-    cat "${1}" | while read line; do
-        nome="$(echo -e "$line" | cut -d"," -f 1)"
-        cpf="$(echo -e "$line" | cut -d"," -f 2)"
-        cpf="$(limpa_numeros $cpf)"
-        telefone="$(echo -e "$line" | cut -d"," -f 3)"
-        email="$(echo -e "$line" | cut -d"," -f 4)"
-        cadastrar_cliente "$nome" "$cpf" "$telefone" "$email"
-
-    done
-}
-
-cadastrar_contatos "$1"
+completar_cadastro
+echo "$(tput setaf 2)[info] Cliente cadastrado com sucesso.$(tput sgr0)"
