bf339a80d1dabd06ccc83ce595686a6c12abb73d
vbraraujo <vbraraujo@yahoo.com.br>
2021-03-21 22:29:04 -0300

    Incluso realpath no arquivo de inicialização

--- crm_cli.sh
+++ crm_cli.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 # Script para inicialização do controle e acompanhamento de relacionamento com cliente
 
-# Informe o local da instalação
-PATH_BASE="/mnt/Arquivos/Nextcloud/RochaAraujo/RA-Compartilhamento/crm-cli"
+PATH_BASE="$(dirname $(realpath $0))"
 
-# Não altere os dados abaixo
 source "${PATH_BASE}/config"
 source "${PATH_BASE}/bash-ui"
 source "${PATH_BASE}/biblioteca_funcoes"
