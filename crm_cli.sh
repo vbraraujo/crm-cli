08ffb49d64afcabf22dd977d70df69659112ed0e
vbraraujo <vbraraujo@yahoo.com.br>
2021-03-21 22:14:53 -0300

    Correções gerais

--- crm_cli.sh
+++ crm_cli.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
-# Script para controle e acompanhamento de relacionamento com cliente
-source "/mnt/Arquivos/Nextcloud/RochaAraujo/RA-Compartilhamento/crm-cli/biblioteca_funcoes"
+# Script para inicialização do controle e acompanhamento de relacionamento com cliente
+
+# Informe o local da instalação
+PATH_BASE="/mnt/Arquivos/Nextcloud/RochaAraujo/RA-Compartilhamento/crm-cli"
+
+# Não altere os dados abaixo
+source "${PATH_BASE}/config"
+source "${PATH_BASE}/bash-ui"
+source "${PATH_BASE}/biblioteca_funcoes"
 
 # Execução
 default
-
-echo "---"
-echo "$(tput setaf 2)[info] Concluído com sucesso. Até logo.$(tput sgr0)"
